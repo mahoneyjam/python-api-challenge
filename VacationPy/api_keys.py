a07d3b23bb9708203d7f077252a6d664187df9f2
mahoneyjam <jmamahoney@gmail.com>
2020-01-04 21:57:29 -0600

    built the DataFrame in WeatherPy

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "4da2fd17f307d2b439e3bde89e78607c"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
---
+++ WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "65ea7f29379aeac203417e0de8c0745b"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
